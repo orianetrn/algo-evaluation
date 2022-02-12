961bd9954a1ec376074b55d7d24dafb433b7d0eb
orianetrn <oriane.tourneroche@gmail.com>
2022-02-12 18:37:01 +0000

    <save25>

--- main/03-difficile.js
+++ main/03-difficile.js
@@ -1,11 +1,11 @@
 /**
  * Exercice - FOU
  * 
- * Programmer une fonction qui prend en paramètres un numéro de ligne et de colonne et renvoie (return) un tableau contenant les paires lignes-colonnes accessibles par
- * un Fou. Comme il avait été fait en cours pour la Tour.
- * Un fou ne se déplace qu'en diagonale, sur toute la longueur du plateau.
+ * Programmer une fonction qui prend en paramètres un numéro de ligne et de colonne et renvoie (return) un tableau contenant les paires 
+ * lignes-colonnes accessibles par un Fou. Comme il avait été fait en cours pour la Tour. Un fou ne se déplace qu'en diagonale, sur toute la longueur du plateau.
  * 
- * Si vous ne connaissez pas le jeu d'échecs, allez chercher une image sur le net. Le plateau se présente comme un ensemble de cases, 8 en ligne et 8 en colonne, donc 64 cases.
+ * Si vous ne connaissez pas le jeu d'échecs, allez chercher une image sur le net. Le plateau se présente comme un ensemble de cases, 8 en ligne et 8 en 
+ * colonne, donc 64 cases.
  * Nous considérerons que les lignes vont de 1 à 8, et idem pour les colonnes. Donc inutile de gérer les colonnes comme des lettres.
  * 
  * Exemple : movementsFou(3, 4) correspond à un Fou placé sur la 3ème ligne et la 4ème colonne.
@@ -15,17 +15,62 @@
  * 
  */
  function mouvementsFou(ligne, colonne) {
+     // monter à droite
+     let t = [[]];
+
+     let i = ligne +1;
+     let j = colonne +1;
+     while (i<=8 && j<=8 ){
+        let ts = [];
+        ts.push(i,j);
+        t.push(ts); 
+        i++;
+        j++;  
+     }
+     //descendre à droite
+     i = ligne +1;
+     j = colonne -1;
+     while (i<=8 && j>=1 ){
+        let ts = [];
+        ts.push(i,j);
+        t.push(ts); 
+        i++;
+        j--;  
+     }
+     // monter à gauche
+     i = ligne -1;
+     j = colonne +1;
+     while (i>=1 && j<=8 ){
+        let ts = [];
+        ts.push(i,j);
+        t.push(ts); 
+        i--;
+        j++;  
+     }
+     // descendre à gauche
+     i = ligne -1;
+     j = colonne -1;
+     while (i>=1 && j>=1 ){
+        let ts = [];
+        ts.push(i,j);
+        t.push(ts); 
+        i--;
+        j--;  
+     }
+
+     return t;
+     /*
     return [
         [ligne - 1, colonne - 1],
         [ligne, colonne],
         [ligne + 1, colonne + 1],
-    ]
+    ]*/
 }
 
 /**
  * Ce log ne fait pas partie de l'exercice, ne vous en préoccupez donc pas, mais il vous aidera à afficher vos tests avec un joli formattage.
  */
- console.log(`Les movements d'un fou sont : ${mouvementsFou(4, 4).map(cell => `[${cell}]`)}`)
+ console.log(`Les movements d'un fou sont : ${mouvementsFou(8, 8).map(cell => `[${cell}]`)}`)
 
 
 
@@ -33,7 +78,8 @@
 
 
 /**
- * Programmer une fonction qui prend en paramètre un nombre, correspondant à la hauteur d'une pyramide et qui renvoie un tableau de caractères "*" et espace afin de construire
+ * Programmer une fonction qui prend en paramètre un nombre, correspondant à la hauteur d'une pyramide et qui renvoie un tableau de caractères "*" et 
+ * espace afin de construire
  * une pyramide avec la hauteur en argument.
  * 
  * Exemple, pour une hauteur de 5 : ["    *    ", "   ***   ", "  *****  ", " ******* ", "*********"]
@@ -49,7 +95,22 @@
  * 
  */
 function pyramide(hauteur) {
-    return ["    *    ", "   ***   ", "  *****  ", " ******* ", "*********"]
-}
+  let t = [];
+  let s = "";
+  let h = hauteur;
 
+  for (let i=0; i<hauteur; i++){
+    for (let j=1; j<=h-1; j++) s = s + " ";
+    s = s + "*";
+    t[i] = s; 
+    h = h -1;
+  }
+  return t;
+    //return ["    *    ", "   ***   ", "  *****  ", " ******* ", "*********"]
+}
 pyramide(5).forEach(e => console.log(`${e}`))
+
+pyramide(10).forEach(e => console.log(`${e}`))
+
+
+pyramide(10).forEach(e => console.log(`${e}`))
